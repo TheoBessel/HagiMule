01126de95b8fcecb845e8562af9599032a97d36d
TheoBessel <theo.bessel@outlook.com>
2025-01-10 15:12:51 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,21 @@ devices=(
 )
 
 #tmux new-session -d -s iode-session "ssh tbl3216@iode -t 'git clone https://github.com/TheoBessel/HagiMule.git --branch wip /work/HagiMule; cd /work/HagiMule; ./gradlew Diary:run'";
+#tmux new-session -d -s $device-session "ssh tbl3216@$device 'rm -rf /work/HagiMule; git clone https://github.com/TheoBessel/HagiMule.git --branch wip /work/HagiMule; cd /work/HagiMule; export IP=\"iode.enseeiht.fr\"; nohup ./gradlew Daemon:run >> /work/hagimule_logs.txt'"
 
 for device in "${devices[@]}"; do
-    tmux new-session -d -s $device-session "ssh tbl3216@$device -t 'git clone https://github.com/TheoBessel/HagiMule.git --branch wip /work/HagiMule; cd /work/HagiMule; export IP=\"iode.enseeiht.fr\"; ./gradlew Daemon:run'";
+    tmux new-session -d -s $device-session "
+        ssh tbl3216@$device '
+            rm -rf /work/HagiMule;
+            git clone https://github.com/TheoBessel/HagiMule.git --branch wip /work/HagiMule;
+            cd /work/HagiMule;
+            ./gradlew jar;
+            export IP=iode.enseeiht.fr;
+            nohup java -jar Daemon/build/libs/Daemon.jar >> /work/hagimule_logs.txt
+        '
+    ";
 done
 
+
+#git clone https://github.com/TheoBessel/HagiMule.git --branch wip /work/HagiMule; cd /work/HagiMule; ./gradlew Diary:run;
+#rm -rf /work/HagiMule; git clone https://github.com/TheoBessel/HagiMule.git --branch wip /work/HagiMule; cd /work/HagiMule; export IP=iode.enseeiht.fr; ./gradlew Daemon:run
