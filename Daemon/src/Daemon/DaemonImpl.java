7e668b065aad74b9bf8051ff5e78810f2c4c20de
Nell15 <nelltruong67@gmail.com>
2025-01-13 19:03:29 +0100

    wip: diary reconnection works WITHOUT downloader

--- Daemon/src/Daemon/DaemonImpl.java
+++ Daemon/src/Daemon/DaemonImpl.java
@@ -1,4 +1,5 @@
 package Daemon;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,26 +26,9 @@ public class DaemonImpl extends UnicastRemoteObject implements Daemon {
             System.out.printf("Daemon started %s:%s\n", hostname, port);
             System.out.println("[===========================================]");
 
-            // Add currently owned files to the diary
-            Path workspaceRoot = Paths.get(System.getProperty("user.dir"));
-            Path downloadDir = Paths.get(workspaceRoot.toString(), "/downloads");
-            try {
-                Stream<Path> files = Files.list(downloadDir);
-                files.forEach(file -> {
-                    try {
-                        Integer socket_port = Integer.parseInt(System.getenv("TCP_PORT"));
-                        notifyFileCreation(file.getFileName().toString(), Files.size(file), socket_port); // Notify Daemon of file creation
-                    } catch (IOException e) {
-                        System.err.println("Error while trying to find the file `" + file.toString() + "` in the file system.");
-                        e.printStackTrace();
-                    }
-                });
-                files.close();
-            }
-            catch (IOException e) {
-                System.err.println("Error while searching for 'downloads' directory");
-                e.printStackTrace();
-            }
+            // Add owned files
+            notifyAllFiles();
+
         } catch (Exception e) {
             System.err.println("Error while starting Daemon component.");
             e.printStackTrace();
@@ -52,16 +36,32 @@ public class DaemonImpl extends UnicastRemoteObject implements Daemon {
     }
 
     @Override
-    public void heartbeat() throws RemoteException {
+    public Integer heartbeat() throws RemoteException {
+        String hostname = System.getenv("RMI_IP");
+        String port = System.getenv("RMI_PORT");
         try {
+            try {
+                this.diary = (Diary) Naming.lookup("//" + hostname + ":" + port + "/Diary");
+            }
+            catch (Exception e) {
+                System.err.println("Reconnecting...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e1) {
+                    System.err.println(e1);
+                }
+            }
             this.diary.heartbeat();
         }
         // If the Diary has disconnected
         catch (RemoteException e) {
-            heartbeat();
-            // has reconnected
-            // add all
+            return Integer.valueOf(0);
         }
+        // catch (Exception e) {
+        //     System.err.println("Error while reconnecting to diary");
+        //     e.printStackTrace();
+        // }
+        return Integer.valueOf(1);
     }
 
     @Override
@@ -74,5 +74,34 @@ public class DaemonImpl extends UnicastRemoteObject implements Daemon {
     public void notifyFileCreation(String name, Long size, Integer port) throws RemoteException {
         System.out.println("Adding file `" + name + "` with size " + size + " to the diary ...");
         this.diary.addFile(new FileInfoImpl(name, size), port);
+    }
+
+    @Override
+    public void notifyAllFiles(){
+        // Add currently owned files to the diary
+        Path workspaceRoot = Paths.get(System.getProperty("user.dir"));
+        Path downloadDir = Paths.get(workspaceRoot.toString(), "/downloads");
+        try {
+            System.out.println("ForEach started");
+            Stream<Path> files = Files.list(downloadDir);
+            files.forEach(file -> {
+                try {
+                    Integer socket_port = Integer.parseInt(System.getenv("TCP_PORT"));
+                    notifyFileCreation(file.getFileName().toString(), Files.size(file), socket_port); // Notify
+                                                                                                      // Daemon of
+                                                                                                      // file
+                                                                                                      // creation
+                } catch (IOException e) {
+                    System.err.println(
+                            "Error while trying to find the file `" + file.toString() + "` in the file system.");
+                    e.printStackTrace();
+                }
+            });
+            files.close();
+            System.out.println("ForEach finished");
+        } catch (IOException e) {
+            System.err.println("Error while searching for 'downloads' directory");
+            e.printStackTrace();
+        }
     }
 }
--- Daemon/src/DaemonMain.java
+++ Daemon/src/DaemonMain.java
@@ -18,8 +18,27 @@ public class DaemonMain {
 
             Integer port = Integer.parseInt(System.getenv("TCP_PORT"));
             try (ServerSocket s = new ServerSocket(port)) {
+                boolean disconnected = false;
                 while (true) {
-                    new Thread(new UploadService(s.accept())).start();
+
+                    // Heartbeats
+                    try {
+                        Integer heartbeatResult = d.heartbeat();
+                        if (heartbeatResult == 0) {
+                            disconnected = true;
+                        }
+                        else if (heartbeatResult == 1 && disconnected) {
+                            disconnected = false;
+                            d.notifyAllFiles();
+                        }
+                    }
+                    catch (NullPointerException e) {
+                        String hostname = System.getenv("RMI_IP");
+                        String portRmi = System.getenv("RMI_PORT");
+                        System.err.println("Error while connecting to diary : " + hostname + ":" + portRmi + " not found");
+                        break;
+                    }
+                    // new Thread(new UploadService(s.accept())).start();
                 }
             }
 
